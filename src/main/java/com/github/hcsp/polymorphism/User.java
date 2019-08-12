5c0ae7614d4ef6b342392321383a3e20f8826b02
liboyang <huagongyilby@126.com>
2019-08-12 10:21:25 +0800

    匿名内部类

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -1,7 +1,9 @@
 package com.github.hcsp.polymorphism;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class User {
     /** 用户ID，数据库主键，全局唯一 */
@@ -26,11 +28,32 @@ public class User {
     // 这里使用了一个NameCollector类，请尝试将它改写成匿名内部类
     // 使得代码更加集中，更加容易阅读
     public static List<String> collectNames(List<User> users) {
-        NameCollector collector = new NameCollector();
-        users.forEach(collector);
-        return collector.getNames();
-    }
+        List<String> names = new ArrayList<>();
+        users.forEach(new Consumer<User>(){
 
+            @Override
+            public void accept(User user) {
+                names.add(user.getName());
+            }
+
+            public List<String> getNames() {
+                return names;
+            }
+        });
+        return names;
+    }
+//    static class NameCollector implements Consumer<User> {
+//        private final List<String> names = new ArrayList<>();
+//
+//        @Override
+//        public void accept(User user) {
+//            names.add(user.getName());
+//        }
+//
+//        public List<String> getNames() {
+//            return names;
+//        }
+//    }
     public static void main(String[] args) {
         List<User> users = Arrays.asList(new User(1, "a"), new User(2, "b"));
         System.out.println(collectNames(users));
