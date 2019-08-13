ef6bb4788e8caa5ce6551eeb99ad7369446f1c21
frank-0908 <2059326740@qq.com>
2019-08-13 23:15:07 +0800

    匿名内部类练习

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -30,7 +30,12 @@ public class User {
     public static List<String> collectNames(List<User> users) {
        // NameCollector collector = new NameCollector();
         List<String> names = new ArrayList<>();
-        users.forEach(user -> names.add(user.getName()));
+        users.forEach(new Consumer<User>(){
+            @Override
+            public void accept(User user) {
+                names.add(user.getName());
+            }
+        });
         return names;
     }
 
