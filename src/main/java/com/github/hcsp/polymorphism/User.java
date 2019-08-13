1b64243f6f64a0e24e0dfdb8224784eeebdd63a6
frank-0908 <2059326740@qq.com>
2019-08-13 23:30:13 +0800

    匿名内部类练习

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -6,10 +6,14 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class User {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -28,9 +32,9 @@ public class User {
     // 这里使用了一个NameCollector类，请尝试将它改写成匿名内部类
     // 使得代码更加集中，更加容易阅读
     public static List<String> collectNames(List<User> users) {
-       // NameCollector collector = new NameCollector();
+        // NameCollector collector = new NameCollector();
         List<String> names = new ArrayList<>();
-        users.forEach(new Consumer<User>(){
+        users.forEach(new Consumer<User>() {
             @Override
             public void accept(User user) {
                 names.add(user.getName());
