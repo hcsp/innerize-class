8046e272b87a9141b3e3456fc9adee6b1c57f7cb
ws1603 <48832455+ws1603@users.noreply.github.com>
2019-10-19 11:51:00 +0800

    Ws branch (#54)
    Ws branch (#54)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -1,13 +1,16 @@
 package com.github.hcsp.polymorphism;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class User {
-    /** 用户ID，数据库主键，全局唯一 */
+    /*** 用户ID，数据库主键，全局唯一*/
     private final Integer id;
 
-    /** 用户名 */
+    /*** 用户名*/
     private final String name;
 
     public User(Integer id, String name) {
@@ -25,10 +28,17 @@ public class User {
 
     // 这里使用了一个NameCollector类，请尝试将它改写成匿名内部类
     // 使得代码更加集中，更加容易阅读
+
     public static List<String> collectNames(List<User> users) {
-        NameCollector collector = new NameCollector();
+        List<String> collectedName = new ArrayList<String>();
+        Consumer<User> collector = new Consumer<User>() {
+            @Override
+            public void accept(User user) {
+                collectedName.add(user.getName());
+            }
+        };
         users.forEach(collector);
-        return collector.getNames();
+        return collectedName;
     }
 
     public static void main(String[] args) {
