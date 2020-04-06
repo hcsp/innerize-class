257772fb86a842413c16293169995be2e8620493
7952126 <15521608275@163.com>
2020-04-06 16:49:06 +0800

    donkey

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -2,12 +2,17 @@ package com.github.hcsp.polymorphism;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class User {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
@@ -28,6 +33,13 @@ public class User {
     public static List<String> collectNames(List<User> users) {
         NameCollector collector = new NameCollector();
         users.forEach(collector);
+
+        new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return true;
+            }
+        };
         return collector.getNames();
     }
 
