b445455853f1706cea6040adf01e8f78223778a3
Ludwig <392559142@qq.com>
2020-11-24 16:46:42 +0800

    fix

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -3,7 +3,6 @@ package com.github.hcsp.polymorphism;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class User {
     /**
