cdefcb0d892862cb8fb1922c7e13016c1f121908
Diesel Niu <niu853273494@qq.com>
2021-01-01 16:40:20 +0800

    innerize class (#185)
    innerize class (#185)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -1,20 +1,25 @@
 package com.github.hcsp.polymorphism;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class User {
-    /** 用户ID，数据库主键，全局唯一 */
+    /**
+     * 用户ID，数据库主键，全局唯一
+     */
     private final Integer id;
 
-    /** 用户名 */
+    /**
+     * 用户名
+     */
     private final String name;
 
     public User(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
-
     public Integer getId() {
         return id;
     }
@@ -26,9 +31,14 @@ public class User {
     // 这里使用了一个NameCollector类，请尝试将它改写成匿名内部类
     // 使得代码更加集中，更加容易阅读
     public static List<String> collectNames(List<User> users) {
-        NameCollector collector = new NameCollector();
-        users.forEach(collector);
-        return collector.getNames();
+        List<String> names = new ArrayList<>();
+        users.forEach(new Consumer<User>() {
+            @Override
+            public void accept(User user) {
+                names.add(user.getName());
+            }
+        });
+        return names;
     }
 
     public static void main(String[] args) {
