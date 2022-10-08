2f2f7981f13825319ef87be3076bba83c9b87a10
wuchuan123 <609833577@qq.com>
2022-10-08 16:29:32 +0800

    3 (#229)
    3 (#229)

--- src/main/java/com/github/hcsp/polymorphism/User.java
+++ src/main/java/com/github/hcsp/polymorphism/User.java
@@ -1,38 +1,49 @@
 package com.github.hcsp.polymorphism;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class User {
-    /** 用户ID，数据库主键，全局唯一 */
-    private final Integer id;
+  /**
+   * 用户ID，数据库主键，全局唯一
+   */
+  private final Integer id;
 
-    /** 用户名 */
-    private final String name;
+  /**
+   * 用户名
+   */
+  private final String name;
 
-    public User(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+  public User(Integer id, String name) {
+    this.id = id;
+    this.name = name;
+  }
 
-    public Integer getId() {
-        return id;
-    }
+  public Integer getId() {
+    return id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    // 这里使用了一个NameCollector类，请尝试将它改写成匿名内部类
-    // 使得代码更加集中，更加容易阅读
-    public static List<String> collectNames(List<User> users) {
-        NameCollector collector = new NameCollector();
-        users.forEach(collector);
-        return collector.getNames();
-    }
+  // 这里使用了一个NameCollector类，请尝试将它改写成匿名内部类
+  // 使得代码更加集中，更加容易阅读
+  public static List<String> collectNames(List<User> users) {
+    List<String> names = new ArrayList<>();
+    users.forEach(new Consumer<User>() {
+      @Override
+      public void accept(User user) {
+        names.add(user.getName());
+      }
+    });
+    return names;
+  }
 
-    public static void main(String[] args) {
-        List<User> users = Arrays.asList(new User(1, "a"), new User(2, "b"));
-        System.out.println(collectNames(users));
-    }
+  public static void main(String[] args) {
+    List<User> users = Arrays.asList(new User(1, "a"), new User(2, "b"));
+    System.out.println(collectNames(users));
+  }
 }
